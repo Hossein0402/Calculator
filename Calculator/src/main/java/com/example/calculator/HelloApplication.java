cdd218ab113e966b4dc085056cce0aeef59ff836
Hossein0402 <h.ahmadi0402@gmail.com>
2024-07-07 05:26:00 -0700

    Calculator project

--- Calculator/src/main/java/com/example/calculator/HelloApplication.java
+++ Calculator/src/main/java/com/example/calculator/HelloApplication.java
@@ -5,12 +5,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(new File("C:\\Users\\dd\\Documents\\GitHub\\Calculator\\Calculator\\src\\main\\resources\\com\\example\\calculator\\hello-view.fxml").toURI().toURL());
         Scene scene = new Scene(fxmlLoader.load(), 324, 524);
         stage.setTitle("Calculator");
         stage.setScene(scene);
